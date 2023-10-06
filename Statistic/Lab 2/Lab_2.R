3d733ff4fef1d3c19859a5e92cc644f97892fdf5
IliaSvetlichnyi <svetlis2000@gmail.com>
2023-10-06 16:03:51 +0400

    new

---
+++ Statistic/Lab 2/Lab_2.R
@@ -0,0 +1,6 @@
+dbinom(3, 20, 0.2)
+dpois(2, 2.2)
+pgeom(2,0.25) # This is the cumulative value of the geom car. at x=2
+punif(3.2, 0.25, 4)
+pexp(2, 0.5) # this is c.d.f for exp. ran. var.
+pnorm(-1.5, -1, 2) # this is c.d.g for norm. ran. var. # 2 is the st. dev. of X
