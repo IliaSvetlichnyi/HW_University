ca4c223c1a6c50c6e8dfc92a2d2f964c0bc8b691
IliaSvetlichnyi <svetlis2000@gmail.com>
2023-10-06 18:37:11 +0400

    new

---
+++ Industrial/Course_Work/Program.cs
@@ -0,0 +1,26 @@
+﻿using Gtk;
+
+class Program
+{
+    static void Main()
+    {
+        Application.Init();
+
+        var win = new Window("Simple Web Browser");
+        win.Resize(800, 600);
+        win.DeleteEvent += (sender, e) => Application.Quit();
+
+        var vbox = new VBox(false, 2);
+
+        var urlEntry = new Entry();
+        urlEntry.Text = "http://";
+
+        vbox.PackStart(urlEntry, false, false, 0);
+
+        win.Add(vbox);
+
+        win.ShowAll();
+
+        Application.Run();
+    }
+}
