1d68c3e91eb8aadd735f3efe34f06ab10559b11a
IliaSvetlichnyi <svetlis2000@gmail.com>
2023-10-07 17:23:59 +0400

    new

--- Industrial/Course_Work/Program.cs
+++ Industrial/Course_Work/Program.cs
@@ -1,26 +1,19 @@
 ﻿using Gtk;
+using SimpleWebBrowser.UI;
+using SimpleWebBrowser.Http;
 
-class Program
+namespace SimpleWebBrowser
 {
-    static void Main()
+    class Program
     {
-        Application.Init();
+        static void Main()
+        {
+            Application.Init();
 
-        var win = new Window("Simple Web Browser");
-        win.Resize(800, 600);
-        win.DeleteEvent += (sender, e) => Application.Quit();
+            var mainWindow = new BrowserWindow();
+            mainWindow.ShowAll();
 
-        var vbox = new VBox(false, 2);
-
-        var urlEntry = new Entry();
-        urlEntry.Text = "http://";
-
-        vbox.PackStart(urlEntry, false, false, 0);
-
-        win.Add(vbox);
-
-        win.ShowAll();
-
-        Application.Run();
+            Application.Run();
+        }
     }
 }
