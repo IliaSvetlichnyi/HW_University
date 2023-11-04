9f427fd9b79f1d76eccfe0a8f7e39394fd99af7b
IliaSvetlichnyi <svetlis2000@gmail.com>
2023-11-04 15:24:32 +0400

    new

--- Biologically/CW/activation.py
+++ Biologically/CW/activation.py
@@ -4,14 +4,20 @@ import numpy as np
 
 # returns value between -1 and 1
 # implement some limitations in order to avoid exceptions of gion value of "x"
+
+
 def logistic(x):
     x = np.clip(x, -500, 500)
     return 1 / (1 + np.exp(-x))
 
 # if input_value is positive returns it, otherwise returns 0
+
+
 def relu(x):
     return np.maximum(0, x)
 
 # returns value between -1 and 1
+
+
 def tanh(x):
     return np.tanh(x)
